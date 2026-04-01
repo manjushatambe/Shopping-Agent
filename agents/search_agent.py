becfc33d946a2ea82a066ac67eee0bd2bae8fe70
manjushatambe <tambemanjusha09@gmail.com>
2026-04-01 12:37:57 +0530

    refactor: improve JSON parsing in search_products function

--- agents/search_agent.py
+++ agents/search_agent.py
@@ -2,6 +2,7 @@ import subprocess
 import json
 import sys
 import os
+from unittest import result
 
 from agents.fallback import fallback_data
 
@@ -18,7 +19,20 @@ def search_products(query):
             encoding="utf-8",   
             errors="ignore"     
             )
-        data = json.loads(result.stdout)
+        #data = json.loads(result.stdout)
+        
+        output = result.stdout.strip()
+        # Find JSON start
+        json_start = output.find("[")
+        if json_start != -1:
+            output = output[json_start:]
+
+        try:
+            data = json.loads(output)
+        except Exception as e:
+            print("JSON parse error:", e)
+            return fallback_data()
+        
         # Normalize scraper output to match fallback format
         return {
             "all": data,
