fdccce4257cbe3db1598909d973c0fa91c79f978
manjushatambe <tambemanjusha09@gmail.com>
2026-04-02 15:48:11 +0530

    app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 # 🔗 Your Render backend URL
 API_URL = "https://shopping-agent-a4rt.onrender.com//search"
 
-st.set_page_config(page_title="ShopHopAI", page_icon="🛒")
+st.set_page_config(page_title="ShopHopAI")
 st.markdown("""
 <h1 style='text-align:center; margin-bottom:5px;'>🛒 ShopHopAI</h1>
 <h4 style='text-align:center; margin-top:0; color:gray;'>(Your Smart Shopping Assistant)</h4>
